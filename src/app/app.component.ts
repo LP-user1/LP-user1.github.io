b43954f788c2c5dc977b38af9476f289fbc8ebd5
LP-user1 <akashbhuvi3@gmail.com>
2023-02-07 15:47:50 +0530

    Feature : Header responsive for xs devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'LP | Portfolio';
+  navToggle(action: boolean): void {
+    if (action === true) {
+      alert('hi');
+    } else {
+      alert('bye');
+    }
+  }
 }
